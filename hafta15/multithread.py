8bd19653dbc33a9975f20617876c584d2a808e4c
Emir Doğan <emir@sufle.io>
2025-05-31 13:43:53 +0300

    hafta 15 multithread

---
+++ hafta15/multithread.py
@@ -0,0 +1,10 @@
+import threading
+
+def coder(number):
+    print(f'Coder ID: {number}')
+
+threads = []
+for k in range(5):
+    t = threading.Thread(target=coder, args=(k,))
+    threads.append(t)
+    t.start()
